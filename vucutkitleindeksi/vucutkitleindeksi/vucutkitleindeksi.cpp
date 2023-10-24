0c96cdffd98e817c61c17155d145ff3d368d20f4
Lokman Baturay Efe <65876412+lokicik@users.noreply.github.com>
2023-10-25 00:45:38 +0300

    vucutkitleindeksi commit

---
+++ vucutkitleindeksi/vucutkitleindeksi/vucutkitleindeksi.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    float boy, kilo;
+    printf("Kilonuzu giriniz (kg): ");
+    scanf("%f", &kilo);
+
+    printf("Boyunuzu giriniz (cm): ");
+    scanf("%f", &boy);
+
+    float result = kilo / ((boy * boy) / 10000);
+    printf("Vucut kitle indeksiniz: %f", result);
+    return 0;
+}
+
