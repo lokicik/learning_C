0888e0541c6a47cfb73e51567a0919d86d17487e
Lokman Baturay Efe <65876412+lokicik@users.noreply.github.com>
2023-10-25 00:50:53 +0300

    daire_yaricap_alan_hesaplama commit

---
+++ daire_yaricap_alan_hesaplama/daire_yaricap_alan_hesaplama/daire_yaricap_alan_hesaplama.cpp
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#define PI 3.14
+#include <iostream>
+#include <math.h>
+
+int main() {
+    printf("Yari capi giriniz: \n");
+    float r;
+    scanf("%f", &r);
+    float alan = 2 * PI * r;
+    float cevre = PI * pow(r, 2);
+    printf("%f yari capli bir dairenin,\nALANI=%f\nCEVRESI=%f\n", r, alan, cevre);
+
+    return 0;
+}
